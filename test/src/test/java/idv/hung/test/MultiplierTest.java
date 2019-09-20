7b77d8171114fc0e2db0fb6cc7f382270c28688f
first <first@DESKTOP-HCT93NL>
2019-09-20 19:06:19 +0800

    add multiplier

---
+++ test/src/test/java/idv/hung/test/MultiplierTest.java
@@ -0,0 +1,14 @@
+package idv.hung.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultiplierTest {
+
+	@Test
+	public void test() {
+		assertEquals(6, new Multiplier().multiplier(3, 2));
+	}
+
+}
