d76b5ad24d9d204fff36ba7fd602de7b0cfc882a
user <user@DESKTOP-HQ7THGB>
2019-09-23 22:48:18 +0800

    AdderTest add new test case.

--- test/src/test/java/idv/hung/test/AdderTest.java
+++ test/src/test/java/idv/hung/test/AdderTest.java
@@ -8,5 +8,6 @@ public class AdderTest extends TestCase {
 	@Test
 	public void test() {
 		assertEquals("3+5 must be equal 8", 8 ,new Adder().add(3,5));
+		assertEquals("96+32 must be equal 128", 128 ,new Adder().add(96,32));
 	}
 }
