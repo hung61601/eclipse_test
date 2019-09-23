c15bc1cad5a851f7c71f72bc1cc19dd666d5a9c2
first <first@DESKTOP-HCT93NL>
2019-09-23 11:43:01 +0800

    add new test case.

--- test/src/test/java/idv/hung/test/DividerTest.java
+++ test/src/test/java/idv/hung/test/DividerTest.java
@@ -11,4 +11,10 @@ public class DividerTest {
 		assertEquals("9/3 must be equal 3", 3 ,new Divider().divide(9, 3)) ;
 	}
 	
+	@Test
+	public void test2() {
+		assertEquals("12/3 must be equal 4", 3 ,new Divider().divide(12, 3)) ;
+	}
+	
+	
 }
