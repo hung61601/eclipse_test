77097623172711b95611836d19c056a902f89018
first <first@DESKTOP-HCT93NL>
2019-09-23 11:40:06 +0800

    add new test case.

--- test/src/test/java/idv/hung/test/SubtractorTest.java
+++ test/src/test/java/idv/hung/test/SubtractorTest.java
@@ -9,6 +9,7 @@ public class SubtractorTest {
 	@Test
 	public void test1() {
 		assertTrue("6-3 must be equal 3", new Subtractor().substarct(6,3) == 3);
+		assertTrue("6-2 must be equal 4", new Subtractor().substarct(6,2) == 4);
 	}
 
 }
