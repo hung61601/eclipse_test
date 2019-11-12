02e34b8013eef52d2101a1aa9015f4a674471683
first <first@DESKTOP-HCT93NL>
2019-11-12 15:21:33 +0800

    organize code

--- test/src/main/java/idv/hung/test/Divider.java
+++
@@ -1,7 +0,0 @@
-package idv.hung.test;
-
-public class Divider {
-	public int divide(int x,int y){
-		return x/y ;
-	}
-}
--- test/src/main/java/idv/hung/test/Subtractor.java
+++
@@ -1,7 +0,0 @@
-package idv.hung.test;
-
-public class Subtractor {
-	public int substarct(int x,int y){
-		return x-y ;
-	}
-}
